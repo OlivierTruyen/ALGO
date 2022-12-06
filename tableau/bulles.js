a926bfdffad80049ba767a45e84138ab88edb57d
OlivierTruyen <truyen.olivier@gmail.com>
2022-12-06 16:31:52 +0100

    modif

--- tableau/bulles.js
+++ tableau/bulles.js
@@ -2,8 +2,39 @@
 
 // Étape 1 : écrire une fonction qui reçoit un tableau et un numéro de case (i). Son but est d'échanger les valeurs des cases i et i+1 si celles-ci ne sont pas dans le bon ordre.
 
-// Étape 2 : écrire une fonction qui reçoit un tableau et qui applique l'étape 1 sur toutes les cases successives (cases 0/1, cases 1/2, cases 2/3, ..., cases avant-dernière/dernière).
+let tab10=[10,8,15,7,9];
+// 8 10 15 7 9
+// 8 10 15 7 9
+// 8 10 7 15 9
+// 8 10 7 9 15
 
+
+function bulle(tableau,i){ 
+    if(tableau[i]>tableau[i+1]){
+        let exchange=tableau[i];
+        tableau[i] = tableau[i+1];
+        tableau[i+1] = exchange;
+    }       
+}
+// Étape 2 : écrire une fonction qui reçoit un tableau et qui applique l'étape 1 sur toutes les cases successives (cases 0/1, cases 1/2, cases 2/3, ..., cases avant-dernière/dernière).
+function bulle2(tab){
+    for(let i=0 ; i<tab.length-1 ; i++){
+        bulle(tab,i);
+
+    }
+
+}
+bulle2(tab10);
+console.log(tab10);
 // Étape 3 : écrire une fonction qui reçoit un tableau et qui le trie en appelant la fonction 2 autant de fois qu'il y a d'éléments dans le tableau.
+function bulle3(tab){
+    for(let i=0 ; i<tab.length-1 ; i++){
+        bulle2(tab,i);
+
+    }
+
+}
+bulle3(tab10);
+console.log(tab10);
 
 // Note : on peut visualiser cet algorithme sur cette image <https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/211162/a9b17b54-b6dd-1602-be04-6fc125676b7a.gif>.
--- tableau/f_tableaux.js
+++ tableau/f_tableaux.js
@@ -1,9 +1,61 @@
 // Exercice all2_1 :	écrire une fonction qui retourne **la position** du plus grand élément d'un tableau.
+let tab1 = [10,20,-30,20,50,-60];
 
+
+function positionBiggest(tab){
+    let position=0;
+
+    for (let i=0 ; i<tab.length ;i++){
+        if(tab[i]>tab[position]){
+            position= i
+
+        }
+
+    }
+    return position;
+}
+console.log(positionBiggest(tab1));
 // Exemple : positionBiggest([4,7,5,5,6]) retourne 1 car le plus grand est dans la case numéro 1
 
 // Exercice all2_2 : écrire une fonction qui fait la somme de tous les éléments d'un tableau.
 
+
+
+function sommeTab(tab){
+    let somme = 0;
+    for(let i=0 ; i<tab.length;i++){
+        somme = somme + tab[i];
+    }
+    return somme
+}
+console.log(sommeTab(tab1));
+
 // Exercice all2_3 : écrire une fonction qui retourne la somme des valeurs positives d'un tableau.
+//  tab1 = [10,20,30,20,50,60];
+
+ function sommePositif(tab){
+    let somme=0;
+    for(let i=0 ; i<tab.length ; i++){
+        if(tab[i]>0){
+            somme= somme + tab[i]
+        }
+
+    }
+    return somme
+ }
+ console.log(sommePositif(tab1));
+
 
 // Exercice all2_4 : En utilisant les deux fonctions précédentes, écrire une fonction qui retourne la somme des valeurs négatives d'un tableau.
+
+function sommeNegative(tab){
+    let somme=0;
+    for(let i=0 ; i<tab.length ; i++){
+        if(tab[i]<0){
+            somme= somme + tab[i]
+        }
+
+    }
+    return somme
+ }
+ console.log(sommeNegative(tab1));
